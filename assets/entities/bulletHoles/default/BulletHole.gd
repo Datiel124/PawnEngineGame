363dbe7c04aaa27af1331b132bb447721ee26b5b
Datiel124 <104692281+Datiel124@users.noreply.github.com>
2024-03-11 03:27:46 -0400

    3

--- assets/entities/bulletHoles/default/BulletHole.gd
+++ assets/entities/bulletHoles/default/BulletHole.gd
@@ -3,8 +3,8 @@ class_name BulletHole
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$decal/impactParticle.emitting = true
-
+	$impactParticle.emitting = true
+	$dirtParticles.emitting = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):

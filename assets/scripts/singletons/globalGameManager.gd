4df3a6bcbeb6444b45092ee55fdd363776ae77a6
Datiel124 <104692281+Datiel124@users.noreply.github.com>
2024-03-18 22:03:20 -0400

    Godot Steam

--- assets/scripts/singletons/globalGameManager.gd
+++ assets/scripts/singletons/globalGameManager.gd
@@ -35,6 +35,8 @@ const dialogue_cam : PackedScene = preload("res://assets/entities/camera/Dialogu
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	initializeSteam()
+
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	soundPlayer.bus = "Sounds"
 	if richPresenceEnabled:
@@ -213,3 +215,11 @@ func playSound(stream):
 func getGlobalSound(soundname: String):
 	if sounds.get(soundname) != null:
 		return sounds.get(soundname)
+
+func initializeSteam():
+	var initialize_response: Dictionary = Steam.steamInitEx()
+	print("Did Steam initialize?: %s " % initialize_response)
+
+	if initialize_response['status'] > 0:
+		print("Failed to initialize Steam, shutting down: %s" % initialize_response)
+		get_tree().quit()
